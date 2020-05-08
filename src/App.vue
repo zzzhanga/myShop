d1ea3349fec205cb89acb81d5d8dc2047624d8f4
jiely007 <2922712734@qq.com>
2020-05-08 16:31:23 +0800

    底部导航栏栏基础组件与基础样式以及挂载完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
 
-    <router-view/>
+    <router-view />
+
+    <!--3 使用组件  -->
+    <!-- 方法一 -->
+
+    <FooterGuide />
+    <!-- 方法二 -->
+    <!-- <footer-guide></footer-guide> -->
+
+    <!-- 引入公共组件 -->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  // 1引入 底部导航栏组件
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default {
+    name: 'App',
+    components: {
+      FooterGuide //2 挂载组件
+    }
+
+  }
+
 </script>
 
 <style>
-
 </style>
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    底部公共组件
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+
+    },
+    data() {
+      return {
+
+      };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+  };
+
+</script>
+
+<style scoped lang='stylus'>
+  div  
+    color red
+    background-color green
+    font-size 30px
+</style>
